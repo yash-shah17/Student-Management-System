44a832ea37dc7dbd160a0c13fc1ceb7f093b412c
yash-shah17 <88420125+yash-shah17@users.noreply.github.com>
2022-09-16 00:54:45 +0530

    updated axios

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -3,59 +3,31 @@ import { Card, Container, Table, ButtonGroup, Button } from "react-bootstrap";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import {Link} from 'react-router-dom'
 
 export default function StudentList(props) {
   const [students, setStudents] = useState([]);
-
-  const notify = (message, hasError = false) => {
-    if (hasError) {
-      toast.error(message, {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    } else {
-      toast(message, {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    }
-  };
 
   useEffect(() => {
     getStudents();
   }, []);
 
-  const getStudents = async () => {
+  let getStudents = () => {
     axios
-      .get("http://localhost:8080/students")
+      .get("http://localhost:8080/listStudents")
       .then((response) => setStudents(response.data))
       .catch((error) => alert(error));
   };
 
   let deleteStudent = (studentId) => {
-    axios
-      .delete(`http://localhost:8080/students/${studentId}`)
-      .then((response) => {
-        if (response.data !== null) {
-          props.showAlert("success", "Record deleted successfully");
-          notify("student record deleted successfully");
-          setStudents(students.filter((student) => student.id !== studentId));
-        }
-      });
-  };
+    axios.delete("http://localhost:8080/student/"+studentId)
+    .then(response=> {
+      if (response.data !== null){
+        props.showAlert("success", "Record deleted successfully")
+        setStudents(students.filter(student=>student.id!==studentId));
+      }
+    })
+  }
 
   return (
     <div className="my-3">
@@ -68,9 +40,8 @@ export default function StudentList(props) {
             <thead>
               <tr>
                 <th>Student Id</th>
-                <th>Student F_Name</th>
-                <th>Student L_Name</th>
-                <th>Student EmailId</th>
+                <th>Student Name</th>
+                <th>Student Address</th>
                 <th>Edit/Delete</th>
               </tr>
             </thead>
@@ -80,53 +51,26 @@ export default function StudentList(props) {
                   <td colSpan={3}>{students.length} Studnets Available!!!</td>
                 </tr>
               ) : (
-                students.map((student) => (
-                  <tr key={student.id}>
-                    <td>{student.id}</td>
-                    <td>{student.f_name}</td>
-                    <td>{student.l_name}</td>
-                    <td>{student.emailId}</td>
-                    <td>
-                      <ButtonGroup>
-                        <Link to={"/student/" + student.id}>
-                          <Button size="sm" variant="outline-primary">
-                            <FontAwesomeIcon icon={faEdit}>
-                              Edit
-                            </FontAwesomeIcon>
-                          </Button>
-                        </Link>
-
-                        <Button
-                          size="sm"
-                          variant="outline-danger"
-                          onClick={deleteStudent.bind(this, student.id)}
-                        >
-                          <FontAwesomeIcon icon={faTrash}>
-                            Delete
-                          </FontAwesomeIcon>
-                        </Button>
-
-                        {/* <Button size="sm" variant="outline-danger" onClick={()=>deleteStudent(student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button> */}
-                      </ButtonGroup>
-                    </td>
-                  </tr>
-                ))
+                students.map((student)=>
+                <tr key={student.id}>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.address}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Link to={"/student/"+student.id}><Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button></Link>{ ' '}
+                      <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this,student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                      {/* <Button size="sm" variant="outline-danger" onClick={()=>deleteStudent(student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button> */}
+                    </ButtonGroup>
+                  </td>
+                </tr>
+                )
               )}
             </tbody>
           </Table>
         </Card.Body>
       </Container>
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
     </div>
   );
 }
+
