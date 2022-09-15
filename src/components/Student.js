d70e834418770c446766f73537173971b9dca09f
yash-shah17 <88420125+yash-shah17@users.noreply.github.com>
2022-09-16 01:00:50 +0530

    removing notifications

--- src/components/Student.js
+++ src/components/Student.js
@@ -14,30 +14,6 @@ export default function Student(props) {
     emailId: "",
   });
 
-  const notify = (message, hasError = false) => {
-    if (hasError) {
-      toast.error(message, {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    } else {
-      toast(message, {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    }
-  };
-
   const { studentId } = useParams(); // Get the Path Parameter from the URL
   const navigate = useNavigate();
 
